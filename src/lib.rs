008cbe595ee282d2bfed529c85af61982a5e0f4f
ayxan <ayxan.20145@gmail.com>
2026-09-24 17:50:45 +0400

    handle quit event

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,16 @@ pub fn create_window() {
     let sdl = sdl3::init().unwrap();
     let video = sdl.video().unwrap();
 
-    let window = video.window("test", 800, 600).build().unwrap();
+    let _window = video.window("test", 800, 600).build().unwrap();
 
-    loop {}
+    let mut event_pump = sdl.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                sdl3::event::Event::Quit { .. } => break 'running,
+                _ => {}
+            }
+        }
+    }
 }
