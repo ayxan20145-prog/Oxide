eafaf8323a49713041b45158a10d8889eec6a17f
ayxan <ayxan.20145@gmail.com>
2026-09-24 19:13:26 +0400

    rename App to Window cuz it made more sense and added methods to it

--- examples/create_window.rs
+++ examples/create_window.rs
@@ -1,18 +1,18 @@
-use oxide::{clear, create_window, draw_rect, handle_events, present};
+use oxide::Window;
 use sdl3::pixels::Color;
 
 fn main() {
-    let mut app = create_window("hi", 800, 600);
+    let mut window = Window::create_window("hi", 800, 600);
 
     loop {
-        if !handle_events(&mut app) {
+        if !window.handle_events() {
             break;
         }
 
-        clear(&mut app, Color::RGB(255, 255, 255));
+        window.clear(Color::RGB(255, 255, 255));
 
-        draw_rect(&mut app, 0, 0, 100, 100, Color::RGB(0, 0, 0));
+        window.draw_rect(0, 0, 100, 100, Color::RGB(0, 0, 0));
 
-        present(&mut app);
+        window.present();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,49 @@
 use sdl3::{EventPump, Sdl, event::Event, init, pixels::Color, rect::Rect, render::Canvas, video};
 
-pub struct App {
+pub struct Window {
     pub sdl: Sdl,
     pub canvas: Canvas<video::Window>,
     pub events: EventPump,
 }
 
-pub fn create_window(title: &str, width: u32, height: u32) -> App {
-    let sdl = init().unwrap();
-    let video = sdl.video().unwrap();
+impl Window {
+    pub fn create_window(title: &str, width: u32, height: u32) -> Self {
+        let sdl = init().unwrap();
+        let video = sdl.video().unwrap();
 
-    let window = video.window(title, width, height).build().unwrap();
+        let window = video.window(title, width, height).build().unwrap();
 
-    let events = sdl.event_pump().unwrap();
-    let canvas = window.into_canvas();
+        let events = sdl.event_pump().unwrap();
+        let canvas = window.into_canvas();
 
-    App {
-        sdl,
-        canvas,
-        events,
-    }
-}
-pub fn handle_events(app: &mut App) -> bool {
-    for event in app.events.poll_iter() {
-        match event {
-            Event::Quit { .. } => return false,
-            _ => {}
+        Window {
+            sdl,
+            canvas,
+            events,
         }
     }
+    pub fn handle_events(&mut self) -> bool {
+        for event in self.events.poll_iter() {
+            match event {
+                Event::Quit { .. } => return false,
+                _ => {}
+            }
+        }
 
-    true
-}
-pub fn clear(app: &mut App, color: Color) {
-    app.canvas.set_draw_color(color);
-    app.canvas.clear();
-}
-pub fn draw_rect(app: &mut App, x: i32, y: i32, width: u32, height: u32, color: Color) {
-    app.canvas.set_draw_color(color);
+        true
+    }
+    pub fn clear(&mut self, color: Color) {
+        self.canvas.set_draw_color(color);
+        self.canvas.clear();
+    }
+    pub fn draw_rect(&mut self, x: i32, y: i32, width: u32, height: u32, color: Color) {
+        self.canvas.set_draw_color(color);
 
-    let rect = Rect::new(x, y, width, height);
+        let rect = Rect::new(x, y, width, height);
 
-    app.canvas.fill_rect(rect).unwrap();
-}
-pub fn present(app: &mut App) {
-    app.canvas.present();
+        self.canvas.fill_rect(rect).unwrap();
+    }
+    pub fn present(&mut self) {
+        self.canvas.present();
+    }
 }
