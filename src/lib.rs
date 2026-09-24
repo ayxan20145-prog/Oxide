51f93368d4e40082fb419f529b7585cd4c852af2
ayxan <ayxan.20145@gmail.com>
2026-09-24 17:53:41 +0400

    change bg color

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub fn create_window() {
     let _window = video.window("test", 800, 600).build().unwrap();
 
     let mut event_pump = sdl.event_pump().unwrap();
+    let mut canvas = _window.into_canvas();
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -12,6 +13,10 @@ pub fn create_window() {
                 sdl3::event::Event::Quit { .. } => break 'running,
                 _ => {}
             }
+
+            canvas.set_draw_color(sdl3::pixels::Color::RGB(255, 255, 255));
+            canvas.clear();
+            canvas.present();
         }
     }
 }
